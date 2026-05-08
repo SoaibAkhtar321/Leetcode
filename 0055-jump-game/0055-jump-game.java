483af77474ba4d9ecea1e10eff81f66b133e650e
SoaibAkhtar321 <akhtarsoaib568@gmail.com>
2026-05-09 01:51:52 +0530

    Time: 2 ms (87.33%), Space: 48.1 MB (22.63%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int fartest =0;
+        for(int i=0; i<nums.length;i++){
+            if(i>fartest) return false;
+
+            fartest = Math.max(fartest , i+nums[i]);
+
+            if(fartest>=nums.length-1) return true;
+        }
+        return true;
+        
+    }
+}
